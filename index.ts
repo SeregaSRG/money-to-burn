fa1c9bdbec1d141cc8299983f1057dbd05864ed8
Sergey <grebenyuk.s@obelis.ltd>
2023-08-03 17:52:50 +0200

    add questions

--- index.ts
+++ index.ts
@@ -11,6 +11,51 @@ const port = process.env.PORT;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+interface MessageFromCLient {
+  type: 'control' | 'answer';
+  uuid: string;
+  payload: string;
+}
+
+const gameQuestions = [
+  [
+    {
+      question: 'Какой город является столицей России?',
+      answer: 'Москва',
+    },
+    {
+      question: 'Какой город является столицей США?',
+      answer: 'Вашингтон',
+    },
+    {
+      question: 'Какой город является столицей Украины?',
+      answer: 'Киев',
+    }
+  ],
+  [
+    {
+      question1: 'Какой город является столицей России?',
+      answer1: 'Москва',
+      question2: 'Какой город является столицей США?',
+      answer2: 'Вашингтон',
+      fullAnswer: 'Москва, Вашингтон',
+    }
+  ],
+  [
+    {
+      question: 'Какой город является столицей России?',
+      answer: 'Москва',
+      hints: [
+        'M____',
+        'Mo___',
+        'Mos__',
+        'Mosc_',
+        'Mosco_',
+      ]
+    }
+  ]
+]
+
 wss.on('connection', (ws: WebSocket) => {
   ws.on('message', (message: string) => {
     console.log(`Received message: ${message}`);
